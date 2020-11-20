317d38e0c6d2c4867ec0ca92fd5112e25fadebd3
kiddigital <kiddigital@gmail.com>
2020-11-20 21:39:03 +0100

    Corrected header references

--- main/WebServerOpenAPI_v2_Services.cpp
+++ main/WebServerOpenAPI_v2_Services.cpp
@@ -2,7 +2,7 @@
   All '/Services' calls of OpenAPI_v2 Webserver are here
 
   This file gets included by the main class from 'WebServerOpenAPI_v2.cpp'
-  All method definitions are in the main header file 'WebServerOpenAPI_v2.h'
+  All method definitions are in the main header file 'WebServerOpenAPI_v2.hpp'
 ******************************************************************************/
 
 void CWebServerOpenAPI_v2::GetServicesStatus(const Json::Value& input, Json::Value& result)
