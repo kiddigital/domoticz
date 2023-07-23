a6e73f73771c760bb7c8717b0dcbe0a7d4c11fa6
kiddigital <kiddigital@gmail.com>
2023-07-23 22:05:07 +0200

    Implemented Service Version

--- main/WebServerOpenAPI_v2_Services.cpp
+++ main/WebServerOpenAPI_v2_Services.cpp
@@ -5,6 +5,31 @@
   All method definitions are in the main header file 'WebServerOpenAPI_v2.h'
 ******************************************************************************/
 
+void CWebServerOpenAPI_v2::GetServicesVersion(const Json::Value& input, Json::Value& result)
+{
+  result["version"] = "hidden";
+
+  if (m_userrights != (int8_t)http::server::URIGHTS_NONE)
+  {
+    result["version"] = szAppVersion;
+    result["hash"] = szAppHash;
+    result["build_time"] = szAppDate;
+    //CdzVents* dzvents = CdzVents::GetInstance();
+    //result["dzvents_version"] = dzvents->GetVersion();
+    result["python_version"] = szPyVersion;
+    result["UseUpdate"] = false;
+    result["HaveUpdate"] = m_mainworker.IsUpdateAvailable(false);
+
+    if (m_userrights == http::server::URIGHTS_ADMIN)
+    {
+      result["UseUpdate"] = g_bUseUpdater;
+      result["DomoticzUpdateURL"] = m_mainworker.m_szDomoticzUpdateURL;
+      result["SystemName"] = m_mainworker.m_szSystemName;
+      result["Revision"] = m_mainworker.m_iRevision;
+    }
+  }
+}
+
 void CWebServerOpenAPI_v2::GetServicesStatus(const Json::Value& input, Json::Value& result)
 {
 	result["status"] = TESTDEFINE;
